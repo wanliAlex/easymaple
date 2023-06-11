a524cc7f3d48d4de324a379a48bc7f138beb656c
Li Wan <wanli19940222@gmail.com>
2023-06-11 14:15:48 +1000

    initial push

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 """The central program that ties all the modules together."""
 
 import time
-from easymaple.modules.bot import Bot
-from easymaple.modules.capture import Capture
-from easymaple.modules.notifier import Notifier
-from easymaple.modules.listener import Listener
-from easymaple.modules.gui import GUI
+import argparse
+from src.easymaple.modules.bot import Bot
+from src.easymaple.modules.capture import Capture
+from src.easymaple.modules.notifier import Notifier
+from src.easymaple.modules.listener import Listener
+from src.easymaple.modules.gui import GUI
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--enable_rune", help="whether or not enable solve rune", default=False, type=bool)
+
+
+ENABLE_RUNE = parser.enable_rune
 
 bot = Bot()
 capture = Capture()
