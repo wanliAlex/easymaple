bd55a0ab00b7d1f04bd9db053c912db512d6c3a8
Li Wan <wanli19940222@gmail.com>
2024-08-06 11:58:14 +1000

    Finish code

--- src/easymaple/modules/capture.py
+++ src/easymaple/modules/capture.py
@@ -1,16 +1,17 @@
 """A module for tracking useful in-game information."""
 
-import time
-import cv2
-import threading
 import ctypes
-import mss
-import mss.windows
-import numpy as np
-from src.easymaple.common import config, utils
+import threading
+import time
 from ctypes import wintypes
-from src.easymaple.common.vkeys import press, key_down, key_up
-import pygetwindow as gw
+
+import cv2
+import numpy as np
+
+from src.easymaple.common import config, utils
+from src.easymaple.modules.PyWindowsScreenCapture import PyWindowsScreenCapture
+from PIL import Image
+
 user32 = ctypes.windll.user32
 user32.SetProcessDPIAware()
 
@@ -35,6 +36,9 @@ MMT_WIDTH = max(MM_TL_TEMPLATE.shape[1], MM_BR_TEMPLATE.shape[1])
 # The player's symbol on the minimap
 PLAYER_TEMPLATE = cv2.imread('assets/player_template.png', 0)
 PT_HEIGHT, PT_WIDTH = PLAYER_TEMPLATE.shape
+
+
+screen_capture = PyWindowsScreenCapture("src/easymaple/modules/WindowsScreenCapture.dll")
 
 
 class Capture:
@@ -74,35 +78,21 @@ class Capture:
 
     def _main(self):
         """Constantly monitors the player's position and in-game events."""
-        
-        mss.windows.CAPTUREBLT = 0
         while True:
             # Calibrate screen capture
-            # handle = user32.FindWindowW(None, 'MapleStory')
-            # rect = wintypes.RECT()
-            # user32.GetWindowRect(handle, ctypes.pointer(rect))
-            # rect = (rect.left, rect.top, rect.right, rect.bottom)
-            # rect = tuple(max(0, x) for x in rect)
-            #
-            # self.window['left'] = rect[0]
-            # self.window['top'] = rect[1]
-            # self.window['width'] = max(rect[2] - rect[0], MMT_WIDTH)
-            # self.window['height'] = max(rect[3] - rect[1], MMT_HEIGHT)
-            #
-            # # Calibrate by finding the bottom right corner of the minimap
+            handle = user32.FindWindowW(None, 'DESKTOP-Q6IJ88D - 远程桌面连接')
+            rect = wintypes.RECT()
+            user32.GetWindowRect(handle, ctypes.pointer(rect))
+            rect = (rect.left, rect.top, rect.right, rect.bottom)
+            rect = tuple(max(0, x) for x in rect)
 
-            window = gw.getWindowsWithTitle('Maplestory')[0]
-            rect = {
-                "top": window.top,
-                "left": window.left,
-                "width": window.width,
-                "height": window.height
-            }
+            self.window['left'] = rect[0]
+            self.window['top'] = rect[1]
+            self.window['width'] = max(rect[2] - rect[0], MMT_WIDTH)
+            self.window['height'] = max(rect[3] - rect[1], MMT_HEIGHT)
 
-            with mss.mss() as self.sct:
-                self.frame = self.screenshot(rect)
-            if self.frame is None:
-                continue
+            # Calibrate by finding the bottom right corner of the minimap
+            self.frame = self.get_game_image()
 
             tl, _ = utils.single_match(self.frame, MM_TL_TEMPLATE)
             _, br = utils.single_match(self.frame, MM_BR_TEMPLATE)
@@ -118,42 +108,40 @@ class Capture:
             self.minimap_sample = self.frame[mm_tl[1]:mm_br[1], mm_tl[0]:mm_br[0]]
             self.calibrated = True
 
-            with mss.mss() as self.sct:
-                while True:
-                    if not self.calibrated:
-                        break
+            while True:
+                if not self.calibrated:
+                    break
 
-                    # Take screenshot
-                    self.frame = self.screenshot(rect)
-                    if self.frame is None:
-                        continue
+                # Take screenshot
+                self.frame = self.get_game_image()
 
-                    # Crop the frame to only show the minimap
-                    minimap = self.frame[mm_tl[1]:mm_br[1], mm_tl[0]:mm_br[0]]
+                # Crop the frame to only show the minimap
+                minimap = self.frame[mm_tl[1]:mm_br[1], mm_tl[0]:mm_br[0]]
 
-                    # Determine the player's position
-                    player = utils.multi_match(minimap, PLAYER_TEMPLATE, threshold=0.8)
-                    if player:
-                        config.player_pos = utils.convert_to_relative(player[0], minimap)
+                # Determine the player's position
+                player = utils.multi_match(minimap, PLAYER_TEMPLATE, threshold=0.8)
+                if player:
+                    config.player_pos = utils.convert_to_relative(player[0], minimap)
 
-                    # Package display information to be polled by GUI
-                    self.minimap = {
-                        'minimap': minimap,
-                        'rune_active': config.bot.rune_active,
-                        'rune_pos': config.bot.rune_pos,
-                        'path': config.path,
-                        'player_pos': config.player_pos
-                    }
+                # Package display information to be polled by GUI
+                self.minimap = {
+                    'minimap': minimap,
+                    'rune_active': config.bot.rune_active,
+                    'rune_pos': config.bot.rune_pos,
+                    'path': config.path,
+                    'player_pos': config.player_pos
+                }
 
-                    if not self.ready:
-                        self.ready = True
-                    time.sleep(0.01)
+                if not self.ready:
+                    self.ready = True
+                time.sleep(1/20)
 
-    def screenshot(self, rect, delay=1):
-        try:
-            return np.array(self.sct.grab(rect))
-        except mss.exception.ScreenShotError:
-            print(f'\n[!] Error while taking screenshot, retrying in {delay} second'
-)
-            time.sleep(delay)
-        time.sleep(0.01)
+    def get_game_image(self, delay=1):
+        data, width, height = screen_capture.capture_monitor(0)
+        image_shape = (height, width, 3)
+        image_array = np.ctypeslib.as_array(data, shape=image_shape)
+        array = np.copy(image_array)
+        game = array[self.window["top"]:self.window["top"] + self.window["height"],
+               self.window["left"]:self.window["left"] + self.window["width"], :]
+        screen_capture.free_all_captures()
+        return game
