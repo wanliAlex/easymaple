2ea47e80093355d29c07f026c3a64cfcf763d59a
Li Wan <li@marqo.ai>
2026-02-08 11:59:05 +0800

    Add 5 seconds sleep between notifications

--- src/easymaple/modules/notifier.py
+++ src/easymaple/modules/notifier.py
@@ -180,6 +180,7 @@ class Notifier:
             while config.bot.rune_active:
                 for _ in range(3):
                     notify(f"<@{DISCORD_USER_ID}> 符文出现了，快去解！")
+                    time.sleep(5)
                 # Wait 30s
                 time.sleep(30)
         finally:
